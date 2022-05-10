1d421fcd6d2f695c20a38d71fb547514ef34d985
YegorOsipov <gogsyan11@gmail.com>
2022-05-10 23:12:16 +0300

    Updates

---
+++ precache-manifest.e4b1d91f51da02bf93913b498e1bbb23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa3a8e61159df8945a7be40323343cb",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "ea990944776c326b7b24",
+    "url": "/simple-react-homeworks/static/css/main.cf6adb3e.chunk.css"
+  },
+  {
+    "revision": "b745ca815cf0ab8bffac",
+    "url": "/simple-react-homeworks/static/js/2.2b6e7bf8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-react-homeworks/static/js/2.2b6e7bf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea990944776c326b7b24",
+    "url": "/simple-react-homeworks/static/js/main.380e08de.chunk.js"
+  },
+  {
+    "revision": "35a9557543d5cb14c0c5",
+    "url": "/simple-react-homeworks/static/js/runtime-main.5a451111.js"
+  }
+]);
